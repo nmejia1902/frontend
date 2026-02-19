73d0ed954ac081e38426d6e7c50f4243b3c3fa33
nmejia1902 <nestornahum@gmail.com>
2026-02-18 19:34:20 -0600

    formateo de archivos

--- contexto/AuthContext.tsx
+++ contexto/AuthContext.tsx
@@ -18,33 +18,33 @@ const [usuario, setUsuario] = useState<string>("");
 const [autenticado, setAutenticado] = useState<boolean>(false); 
 const router = useRouter(); 
 
-const iniciarSesion = (user: string, pass: string) => { 
-if(user === "admin" && pass === "admin123"){ 
-setUsuario(user); 
-setAutenticado(true); 
-router.push("/presupuesto") 
-}else{ 
-alert("credenciales incorrectas") 
-} 
-} 
+    const iniciarSesion = (user: string, pass: string) => {
+        if (user === "admin" && pass === "admin123") {
+            setUsuario(user);
+            setAutenticado(true);
+            router.push("/presupuesto")
+        } else {
+            alert("credenciales incorrectas")
+        }
+    }
 
-const cerrarSesion = () => { 
-setUsuario(""); 
-setAutenticado(false); 
-router.push("/login") 
-} 
+    const cerrarSesion = () => {
+        setUsuario("");
+        setAutenticado(false);
+        router.push("/login")
+    }
 
-return( 
-<AuthContext.Provider value={{usuario, autenticado, iniciarSesion, cerrarSesion}}> 
-{children} 
-</AuthContext.Provider> 
-) 
-} 
+    return (
+        <AuthContext.Provider value={{ usuario, autenticado, iniciarSesion, cerrarSesion }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
 
-export const useAuth = () => { 
-const context = useContext(AuthContext) 
-if(!context){ 
-throw new Error("useAuth debe usarse dentro de AuthProvider")
-} 
-return context 
+export const useAuth = () => {
+    const context = useContext(AuthContext)
+    if (!context) {
+        throw new Error("useAuth debe usarse dentro de AuthProvider")
+    }
+    return context
 } 
